123b87ddabf60da4ec612b359f6aadd92a9e978c
PCovered <dereckyao1006@gmail.com>
2023-09-06 11:01:03 +0800

    UPDATE

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// print 9*9
+for (int i=1;i<=9;i++){
+    for (int j=1;j<=i;j++){
+        Console.Write("{0}*{1}={2}\t",j,i,i*j);
+    }
+    Console.WriteLine();
+}
