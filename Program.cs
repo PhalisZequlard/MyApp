48d22370a146392f77fd29d11a935ffa586df6dc
PCovered <dereckyao1006@gmail.com>
2023-09-10 12:30:29 +0800

    UPDATE

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Worldddd!");
-Console.WriteLine("The current time is " + DateTime.Now);
+﻿// dotnet
+// dotnet new console -o MyApp -f net7.0
+// cd MyApp
+// dotnet run
+
+
+Console.WriteLine("Hello, World!");
+// string afriend = "YEEET";
+// string bfriend = "YeeEET";
+// Console.WriteLine($"Hello {afriend} and {bfriend}");
+// Console.WriteLine($"frienda: {afriend} has {afriend.Length} characters");
+// Console.WriteLine($"friendb: {bfriend} has {bfriend.Length} characters");
+
+
+// string greeting = "   Hello World!   ";
+// Console.WriteLine($"[{greeting}]");
+// string trimmedGreeting = greeting.TrimStart();
+// Console.WriteLine($"[{trimmedGreeting}]");
+// trimmedGreeting = greeting.TrimEnd();
+// Console.WriteLine($"[{trimmedGreeting}]");
+// trimmedGreeting = greeting.Trim();
+// Console.WriteLine($"[{trimmedGreeting}]");
+
+
+// string sayHello = "Hello lllllll!";
+// Console.WriteLine(sayHello);
+
+// sayHello = sayHello.Replace("Hello", "Greetings");
+// Console.WriteLine(sayHello);
